bf263501f96dfc6fe0c8fc0994674a0fb920197e
Dinesh S <99319309+dineshsivan347@users.noreply.github.com>
2026-01-07 14:49:07 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,9 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173, // Default vite port, user can override
-  }
+  },
+  // SET THE BASE URL FOR GITHUB PAGES DEPLOYMENT
+  // If deploying to https://<USERNAME>.github.io/<REPO>/, set base to '/<REPO>/'
+  // If deploying to https://<USERNAME>.github.io/, set base to '/'
+  base: '/',
 })
